bf8fddad76365074aa19c2d50afb7d4a5f8be89e
Georg Maißer <georg.maisser@wunderbyte.at>
2026-03-19 08:59:28 +0100

    Version (2026031900)

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_linkeduser';
 $plugin->release = '1.3.0';
-$plugin->version = 2024112500;
+$plugin->version = 2026031900;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
